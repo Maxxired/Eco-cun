4660887de0314f7728e15092f3650107e39849d5
Alexis Daniel Dolores <alexisdanieldr@gmail.com>
2025-10-16 16:18:13 -0500

    prueba del ruleset

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -133,7 +133,7 @@ export default function Layout({
                 )}
             </nav>
 
-            {/* CONTENIDO */}
+            {/* CONTENIDO cambio cambioso*/}
             <main id="content" className="min-h-[calc(100dvh-3.5rem)]">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <Outlet />
