c51266653f99088f3602064cd2d81e33605ede06
TUL4MAN <199774914+TUL4MAN@users.noreply.github.com.>
2025-11-09 20:21:52 -0500

    Creacion del apartado mis reportes

--- src/app/router/FunctionRoutes.tsx
+++ src/app/router/FunctionRoutes.tsx
@@ -1,9 +1,43 @@
 import type { RouteObject } from "react-router-dom";
+
+// --- IMPORTA TODAS TUS VISTAS AQUÍ ---
 import EcoaportaForm from "../Components/EcoaportaForm";
+import MapView from "../pages/Maps/Map";
+import Campaigns from "../pages/Campaigns/campaigns";
+import OptionsView from '../pages/Options/OptionsView';
+import ReportsView from '../pages/Reports/ReportsView'; // La de reportes
 
 export const FunctionRoutes: RouteObject[] = [
+  // --- Tu ruta EcoAporta ---
   {
-    path: "/ecoaporta",
+    path: "ecoaporta",
     element: <EcoaportaForm />,
+  },
+
+  // --- Las rutas que movimos de HomeRoutes ---
+  {
+    // SIN EL '/' y corregido
+    path: "mapa",
+    element: <MapView />
+  },
+  {
+    // SIN EL '/' y corregido
+    path: "campanas",
+    element: <Campaigns />
+  },
+  {
+    // SIN EL '/'
+    path: 'opciones',
+    element: <OptionsView />,
+  },
+
+  // --- Las nuevas de reportes ---
+  {
+    path: 'mis-reportes',
+    element: <ReportsView />,
+  },
+  {
+    path: 'reportes/:folioId',
+    element: <div>Aquí va el detalle del reporte</div>
   },
 ];
--- src/app/router/HomeRoutes.tsx
+++ src/app/router/HomeRoutes.tsx
@@ -1,25 +1,9 @@
-import type { RouteObject } from "react-router-dom";
+ import type { RouteObject } from "react-router-dom";
 import Home from "../pages/Home/Home";
-import MapView from "../pages/Maps/Map";
-import Campaigns from "../pages/Campaigns/campaigns";
-import OptionsView from '../pages/Options/OptionsView.tsx';
+
 export const HomeRoutes: RouteObject[] = [
     {
         index: true,
         element: <Home />
     },
-    {
-        path: "/maps",
-        element: <MapView />
-
-    },
-    {
-        path: "/campaings",
-        element: <Campaigns />
-
-    },
-    {
-        path: 'opciones',
-        element: <OptionsView />,
-    },
-]
+];
